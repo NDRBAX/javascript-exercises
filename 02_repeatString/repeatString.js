906bae585512b2fb486088c3946f610dc9590b75
NDRBAX <andre.bacaliu@icloud.com>
2022-02-09 22:40:41 +0100

    First try to resolve the first part of this exercice.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
+let string = 'hey';
+let num = 3;
 
+const repeatString = function() {
+    return string.repeat(num);
 };
+
+console.log(repeatString(string, num));
 
 // Do not edit below this line
 module.exports = repeatString;
